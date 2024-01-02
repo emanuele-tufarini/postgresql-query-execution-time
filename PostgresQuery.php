f907b770a6f96d110dad1c008a373d7286c84698
Emanuele Tufarini <emanuele.tufarini@live.com>
2024-01-02 14:10:51 +0100

    AGGIORNATO: classe per eseguire query su un database PostgreSQL

--- PostgresQuery.php
+++ PostgresQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file contains a class for executing queries on a PostgreSQL database.
+ * It also calculates the execution time of the query.
+ */
+
 /** credenziali di accesso al database postgres */
 require "Connection.php";
 
@@ -8,7 +13,17 @@ $query = file_get_contents("Query.sql");
 
 /** classe per testare le query sul database postgres */
 class PostgresQuery {
-    /** effettua la query al database postgres */
+    /**
+     * Executes the query on the PostgreSQL database.
+     * 
+     * @param string $query The SQL query to be executed.
+     * @param string $host The host name of the PostgreSQL server.
+     * @param int $port The port number of the PostgreSQL server.
+     * @param string $dbname The name of the PostgreSQL database.
+     * @param string $user The username for connecting to the PostgreSQL server.
+     * @param string $password The password for connecting to the PostgreSQL server.
+     * @return PDOStatement The result of the query execution.
+     */
     public function Query($query, $host, $port, $dbname, $user, $password) {
         $conn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
         $pdo = new PDO($conn);
@@ -17,7 +32,18 @@ class PostgresQuery {
         //$json = json_encode($rows);
         return $res;
     }
-    /** calcola il tempo di esecuzione */
+    
+    /**
+     * Calculates the execution time of the query.
+     * 
+     * @param string $query The SQL query to be executed.
+     * @param string $host The host name of the PostgreSQL server.
+     * @param int $port The port number of the PostgreSQL server.
+     * @param string $dbname The name of the PostgreSQL database.
+     * @param string $user The username for connecting to the PostgreSQL server.
+     * @param string $password The password for connecting to the PostgreSQL server.
+     * @return float The execution time of the query in seconds.
+     */
     public function Time($query, $host, $port, $dbname, $user, $password) {
         global $query, $host, $port, $dbname, $user, $password;
         $time_start = microtime(true); 
